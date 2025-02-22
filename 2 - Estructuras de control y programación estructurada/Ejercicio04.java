fb0c4d7247e03511c917c8ebe1db1facb7fd65a6
olaguna7 <73069500+olaguna7@users.noreply.github.com>
2025-02-22 09:03:50 +0100

    Update Ejercicio04.java

--- 2 - Estructuras de control y programación estructurada/Ejercicio04.java
+++ 2 - Estructuras de control y programación estructurada/Ejercicio04.java
@@ -5,7 +5,7 @@ un entero que se lee por teclado.
 
 import java.util.Scanner;
 
-public class Ejercicio4 {
+public class Ejercicio04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
