551d4086e7d0783b389ee0ff4b745b478a2e7da6
olaguna7 <73069500+olaguna7@users.noreply.github.com>
2025-02-22 09:04:03 +0100

    Update Ejercicio05.java

--- 2 - Estructuras de control y programación estructurada/Ejercicio05.java
+++ 2 - Estructuras de control y programación estructurada/Ejercicio05.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ejercicio5 {
+public class Ejercicio05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
