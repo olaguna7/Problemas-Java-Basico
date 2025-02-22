dcdd410ebb00d313093ace6b682658f1fe7ddfe2
olaguna7 <73069500+olaguna7@users.noreply.github.com>
2025-02-22 09:06:25 +0100

    Update and rename Ejercicio8.java to Ejercicio08.java

--- 3 - Abstracción procedimental y de datos/Ejercicio08.java
+++ 3 - Abstracción procedimental y de datos/Ejercicio08.java
@@ -4,7 +4,7 @@ Si lo es, el metodo devolvera el siguiente número impar. Si el número no es im
 devolverá el número impar anterior.
  */
 
-public class Ejercicio8 {
+public class Ejercicio08 {
     private static int esImpar(int numero) {
         if (numero % 2 != 0) { // El numero es impar
             return numero + 2;
